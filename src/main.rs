82e7f64326c34c095411d781e40da63b13683f7d
Moritz Baeumel <moritzbaeumnel@gmail.com>
2024-03-03 10:48:11 +0100

    add styling

--- src/main.rs
+++ src/main.rs
@@ -40,12 +40,21 @@ fn TotalWeightView(cx: Scope) -> Element {
 
 fn App(cx: Scope) -> Element {
     render! {
-        HydrationSliderView { }
-        TotalWeightView { }
+        div {
+            class: "userInput",
+            HydrationSliderView { }
+            TotalWeightView { }
+        }
     }
 }
 
 fn main() {
+    dioxus_desktop::launch_cfg(
+        App,
+        dioxus_desktop::Config::new()
+            .with_custom_head(r#"<link rel="stylesheet" href="style/input.css">"#.to_string()),
+    );
+
     // launch the dioxus app in a webview
     dioxus_desktop::launch(App);
 }
